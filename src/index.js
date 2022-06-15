d0395ef7ca7648f5eea699c28fcb2ea1346d7185
Ana Clara Matos <anasmatos28@gmail.com>
2022-06-14 22:14:28 -0300

    a

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './components/App/'
+
+import "./assets/reset.css"
+import "./assets/style.css"
+
+ReactDOM.render(<App/>, document.querySelector(".root"));
