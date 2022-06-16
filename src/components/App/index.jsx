575dc4e1dc30fefad13ef1a4107717473088d0c2
Manuella Dias Cruz <manuelladcsilvacruz@gmail.com>
2022-06-16 10:56:33 -0300

    feat: SignIn and SignUp backend integration

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,21 +5,24 @@ import "../../assets/index.css";
 import "../../assets/query.css";
 
 import { Provider } from "../../hooks/UserContext";
+import { AuthenticationProvider } from "../../hooks/AuthContext";
 import Timeline from "../Timeline";
 import SignUp from "../SignUp";
 import SignIn from "../SignIn";
 
 function App() {
   return (
-    <Provider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/timeline" element={<Timeline />} />          
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <AuthenticationProvider>
+      <Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/timeline" element={<Timeline />} />          
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </AuthenticationProvider>
   );
 }
 
